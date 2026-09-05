84302dd3e7d989a457e252b85fb24adbe0f93262
ariful amar <ariful.amar@gmail.com>
2026-09-05 09:59:20 +0700

    Update cache version to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'timestamp-camera-gps-v3';
+const CACHE = 'timestamp-camera-gps-v4';
 const ASSETS = [
   './', './index.html', './style.css', './app.js', './manifest.webmanifest',
   './icon-192.png', './icon-512.png'
